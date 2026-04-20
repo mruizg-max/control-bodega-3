7129dee7454b7543acd8dc17656802de3b3c66fb
mruizg-max <mruizg@liverpool.com.mx>
2026-04-20 15:52:09 -0600

    Update server.js

--- Control-Bodega-3/server.js
+++ Control-Bodega-3/server.js
@@ -11,11 +11,8 @@ app.use(express.static('public'));
 // 1. TU CONEXIÓN (Pega aquí tu link de MongoDB Atlas)
 const mongoURI = "mongodb+srv://mruizg_db_user:a7jmca7XAYdmVrST@miproyectologistico.v8cdw8u.mongodb.net/ControlBodega?retryWrites=true&w=majority";
 
-mongoose.connect(mongoURI)
-  .then(() => {
-    console.log("✅ Conexión exitosa a MongoDB Atlas");
-    inicializarUsuarios();
-  })
+mongoose.connect('mongodb://mruizg_db_user:mruizg_db_password@cluster0-shard-00-00.v8cdw8u.mongodb.net:27017,cluster0-shard-00-01.v8cdw8u.mongodb.net:27017,cluster0-shard-00-02.v8cdw8u.mongodb.net:27017/controlBodega?ssl=true&replicaSet=atlas-shard-0&authSource=admin&retryWrites=true&w=majority')
+
   .catch(err => console.error("❌ Error de conexión:", err));
 
 // 2. MODELOS DE DATOS
